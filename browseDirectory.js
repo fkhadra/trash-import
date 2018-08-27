b925aeaf1dd22f98b8bae7c4d16b77b002f8bd25
Fadi Khadra <fdkhadra@gmail.com>
2018-08-27 22:40:03 +0200

    Add exclusion

--- browseDirectory.js
+++ browseDirectory.js
@@ -1,16 +1,29 @@
 const { readdirSync, lstatSync } = require('fs');
-const { join } = require('path');
+const { join, extname, basename } = require('path');
 
 const tree = [];
 
+const directoryToExclude = [
+  '.git',
+  'node_modules',
+  // yep still exists
+  '.svn'
+];
+
+const extensionToExclude = [
+  '.json',
+  '.lock',
+  '.map'
+];
+
 function browseDirectory(dir) {
-  for (const file of readdirSync(dir)) {
-    const currentPathname = join(dir, file);
+  for (const pathname of readdirSync(dir)) {
+    const currentPathname = join(dir, pathname);
     const stat = lstatSync(currentPathname);
 
-    if (stat.isFile()) {
+    if (stat.isFile() && !extensionToExclude.includes(extname(pathname))) {
       tree.push(currentPathname);
-    } else if (stat.isDirectory()) {
+    } else if (stat.isDirectory() && !directoryToExclude.includes(basename(pathname))) {
       browseDirectory(currentPathname);
     }
   }
