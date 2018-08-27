07c1f7053330cac3daf05c0aaa3667413d10c957
Fadi Khadra <fdkhadra@gmail.com>
2018-08-27 23:05:48 +0200

    Can save output and silent output

--- index.js
+++ index.js
@@ -1,58 +1,116 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { existsSync } = require('fs');
+const { existsSync, writeFile } = require('fs');
 const { basename, extname, resolve } = require('path');
 const browseDirectory = require('./browseDirectory');
 const scanImport = require('./scanImport');
 
 program
   .arguments('<directory>')
-  .description(`Look 👀 for unused import and remove 💩 the files if you want to.
-  src/index.js and lib/index.js are ignored.
-  `)
-  .option('--delete', 'Delete unused import. BACKUP YOUR FILES FIRST')
-  .action(async (directory) => {
-    if (! existsSync(directory)) {
-      throw new Error("The path provided doesn't match an existing directory")
+  .description(
+    `Look 👀 for unused import and remove 💩 the files if you want to.`
+  )
+  .option('-d, --delete', 'Delete unused import. BACKUP YOUR FILES FIRST')
+  .option('-o, --output <pathname>', 'Save output to json file')
+  .option('-s, --silent', 'Disable output')
+  // See later if needed
+  // .option(
+  //   '-e, --exclude <exclude>',
+  //   'List of files or directory comma separeted to exclude',
+  //   pathname => pathname.split(',').map(path => resolve(path))
+  // )
+  .action(async directory => {
+    if (!existsSync(directory)) {
+      throw new Error("The path provided doesn't match an existing directory");
     }
 
-    const tree = browseDirectory(directory);
-    const imported = await scanImport(tree);
+    const directoryTree = browseDirectory(directory);
+    const importedFiles = await scanImport(directoryTree);
 
-    console.log('Number of files:', tree.length);
-    console.log('Number of unique import:', imported.size);
+    // hardcoded value for the moment
+    // if (program.exclude) {
+    //   console.log('Excluded:');
+    //   for (const file of program.exclude) {
+    //     console.log('-', file)
+    //   }
+    // }
 
-    const toDelete = new Set(
-      tree.filter(pathname => {
+    const filesToDelete = new Set(
+      directoryTree.filter(pathname => {
         const fileName = basename(pathname);
 
-        if (pathname.endsWith('src/index.js') || pathname.endsWith('lib/index.js')) {
+        if (
+          pathname.endsWith('src/index.js') ||
+          pathname.endsWith('lib/index.js')
+        ) {
           return false;
         } else if (fileName === 'index.js') {
-          return !imported.has(pathname.replace('/' + fileName, ''));
+          return !importedFiles.has(pathname.replace('/' + fileName, ''));
         } else {
-          return !imported.has(pathname.replace(extname(fileName), ''));
+          return !importedFiles.has(pathname.replace(extname(fileName), ''));
         }
       })
     );
 
-    console.log('Number of files to delete:', toDelete.size)
-    
-    for (const pathname of toDelete) {
+    const reportToDelete = [];
+    const reportToCheck = [];
+
+    for (const pathname of filesToDelete) {
       const absolutePathname = resolve(pathname);
-      const fileName = basename(pathname);
-
-      console.log('-', absolutePathname);
-
-      if (fileName === 'index.js') {
-        console.log('⚠️', `Directory ${absolutePathname.replace(fileName, '')} could be removed`, '⚠️')
-      }
+      reportToDelete.push(absolutePathname);
 
       if (program.delete) {
         fs.unlinkSync(absolutePathname);
       }
     }
+
+    if (! program.silent) {
+      console.log('Number of files:', directoryTree.length);
+      console.log('Number of unique import:', importedFiles.size);
+      console.log('Number of files to delete:', filesToDelete.size);
+
+      for (const pathname of reportToDelete) {
+        const fileName = basename(pathname);
+        console.log('-', pathname);
+
+        if (fileName === 'index.js') {
+          const directory = pathname.replace(
+            fileName,
+            ''
+          );
+          reportToCheck.push(directory);
+
+          console.log(
+            '⚠️',
+            `Directory ${directory} could be removed`,
+            '⚠️'
+          );
+        }
+      }
+    }
+
+    if (program.output) {
+      const report = {
+        directory,
+        deleteFromDisk: program.delete ? true : false,
+        files: directoryTree.length,
+        uniqueImport: importedFiles.size,
+        toDelete: {
+          files: filesToDelete.size,
+          list: reportToDelete,
+          check: reportToCheck
+        }
+      };
+
+      writeFile(program.output, JSON.stringify(report, null, 4), (err) => {
+        if (err) {
+          throw new Error(err);
+        }
+      });
+    }
+
+
   })
   .parse(process.argv);
 
